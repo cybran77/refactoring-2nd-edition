085929aa5aa430301bdfc0fe6ce865dfa9ef409f
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-05-28 14:33:29 +0800

    Separate tests

--- src/ch01/statement.mjs
+++ src/ch01/statement.mjs
@@ -1,32 +1,6 @@
 import createStatementData from "./createStatementData.mjs";
 
-var invoice = {
-    "customer": "BigCo",
-    "performances": [
-        {
-            "playID": "hamlet",
-            "audience": 55
-        },
-        {
-            "playID": "as-like",
-            "audience": 35
-        },
-        {
-            "playID": "othello",
-            "audience": 40
-        }
-    ]
-};
-
-var plays = {
-    "hamlet": {"name": "Hamlet", "type": "tragedy"},
-    "as-like": {"name": "As You Like It", "type": "comedy"},
-    "othello": {"name": "Othello", "type": "tragedy"}
-};
-
-console.log(statement(invoice, plays));
-
-function statement (invoice, plays) {
+export default function statement (invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
