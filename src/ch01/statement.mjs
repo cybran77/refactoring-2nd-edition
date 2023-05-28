59ebc0728c988b57b51e57db8a8dd57834961812
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-05-28 14:28:30 +0800

    Extract createStatementData and related function

---
+++ src/ch01/statement.mjs
@@ -0,0 +1,49 @@
+import createStatementData from "./createStatementData.mjs";
+
+var invoice = {
+    "customer": "BigCo",
+    "performances": [
+        {
+            "playID": "hamlet",
+            "audience": 55
+        },
+        {
+            "playID": "as-like",
+            "audience": 35
+        },
+        {
+            "playID": "othello",
+            "audience": 40
+        }
+    ]
+};
+
+var plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+};
+
+console.log(statement(invoice, plays));
+
+function statement (invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber/100);
+}
