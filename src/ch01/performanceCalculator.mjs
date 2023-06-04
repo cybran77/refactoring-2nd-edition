a3f38b52dbba215abb28694c1934f2e98ec983ca
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-06-04 14:39:18 +0800

    Move function to class PerformanceCalculator

--- src/ch01/performanceCalculator.mjs
+++ src/ch01/performanceCalculator.mjs
@@ -1,6 +1,28 @@
 export class PerformanceCalculator {
-  constructor(aPerformance, aPlay) {
-    this.performance = aPerformance;
-    this.play = aPlay;
-  }
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    getAmount() {
+        let result = 0;
+        switch (this.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result;
+    }
 }
