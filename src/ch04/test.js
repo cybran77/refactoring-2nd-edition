4ffa6bb3f31927f6c5568b458a0e315707129d59
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-07-01 10:25:23 +0800

    Add test fixture

--- src/ch04/test.js
+++ src/ch04/test.js
@@ -14,14 +14,17 @@ function sampleProvinceData() {
   };
 }
 
-describe('province', function () {    
-    it('shortfall', function () {
-        const asia = new Province(sampleProvinceData());
-        expect(asia.shortfall).equal(5);
-    })
+describe('province', function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
 
-    it('profit', function () {
-        const asia = new Province(sampleProvinceData());
-        expect(asia.profit).equal(230);
-    });
+  it('shortfall', function () {
+    expect(asia.shortfall).equal(5);
+  })
+
+  it('profit', function () {
+    expect(asia.profit).equal(230);
+  });
 });
