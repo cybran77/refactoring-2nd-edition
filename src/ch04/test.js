da55d09d186becdf497eb6d6e98ce89b9101b694
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-07-01 10:45:47 +0800

    Add no producers tests

--- src/ch04/test.js
+++ src/ch04/test.js
@@ -27,4 +27,31 @@ describe('province', function () {
   it('profit', function () {
     expect(asia.profit).equal(230);
   });
+
+  it('change production', function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).equal(-6);
+    expect(asia.profit).equal(292);
+  });  
+});
+
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: 'No producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+
+  it('shortfall', function () {
+    expect(noProducers.shortfall).equal(30);
+  });
+
+  it('profit', function () {
+    expect(noProducers.profit).equal(0);
+  });
 });
